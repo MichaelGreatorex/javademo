b48a308f5d8e13194a13c3361eaf2926c319b3fd
Michael Greatorex <magreatorex@gmail.com>
2024-01-15 17:54:18 +0000

    conditional logic

--- hello.java
+++ hello.java
@@ -1,9 +1,15 @@
 class Hello
 {
-    public static void main(String args[])
+    public static void main(String [] args)
     {
-        int a = 12;
-        byte k = (byte) a;
-        System.out.println(k);
+        int x = 3;
+        int y = 5;
+        boolean result = x < y;
+        if (result == false)
+        {
+            System.out.println("Wrong! x is greater than y");
+        }
+        System.out.println("Correct! x is greater than y");
+        
     }
 }
